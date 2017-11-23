7c28cc15dc4bcfae668c08ff5cbf11afaa4f6869
manprit-sahota <manpritsinghsahota@gmail.com>
2017-11-23 09:29:00 +0530

    Updated screen design with latest metro theme

--- Splendid.POCO.UI/Controls/CustomTextBox.cs
+++ Splendid.POCO.UI/Controls/CustomTextBox.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Splendid.POCO.UI.Controls
 {
@@ -20,19 +21,24 @@ namespace Splendid.POCO.UI.Controls
 
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
+            Foreground = new SolidColorBrush(Colors.LightGray);
             Text = PlaceHolder;
         }
 
         private void OnLostFocus(object sender, RoutedEventArgs e)
         {
             if (string.IsNullOrEmpty(Text) || string.IsNullOrWhiteSpace(Text))
+            {
+                Foreground = new SolidColorBrush(Colors.LightGray);
                 Text = PlaceHolder;
+            }
         }
 
         private void OnGotFocus(object sender, RoutedEventArgs e)
         {
             if (PlaceHolder.Equals(Text, StringComparison.InvariantCultureIgnoreCase))
                 Text = string.Empty;
+            Foreground = new SolidColorBrush(Colors.Black);
         }
 
         public string PlaceHolder
